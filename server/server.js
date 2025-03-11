a4028df7b162089c76c245d2f4abd842115f0196
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-03-11 09:58:23 -0400

    setup project

---
+++ server/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
+import 'dotenv/config';
+
+mongoose
+  .connect(process.env.MONGO_URL)
+  .then(() => console.log('mongodb connected'))
+  .catch((error) => console.log(error));
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(cookieParser());
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));

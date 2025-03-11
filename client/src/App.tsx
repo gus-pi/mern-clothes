b2d3203004a3c56233ed068f53a9ba223884e3c7
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-03-11 11:26:19 -0400

    create auth pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import AuthLayout from './components/ui/auth/layout';
+import LoginPage from './pages/auth/LoginPage';
+import RegisterPage from './pages/auth/RegisterPage';
 
 function App() {
-  return <></>;
+  return (
+    <div className="flex flex-col overflow-hidden bg-white">
+      <h1>header</h1>
+      <Routes>
+        <Route path="/auth" element={<AuthLayout />}>
+          <Route path="login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+        </Route>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;

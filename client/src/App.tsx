7abbe6c9d6fcdddfaaf67360b9aef8d97750ca10
PieMenard <110061246+PieMenard@users.noreply.github.com>
2025-03-11 12:44:26 -0400

    add admin pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import AuthLayout from './components/ui/auth/layout';
+import AuthLayout from './components/auth/layout';
 import LoginPage from './pages/auth/LoginPage';
 import RegisterPage from './pages/auth/RegisterPage';
+import AdminLayout from './components/admin-view/layout';
+import AdminDashboard from './pages/admin-view/AdminDashboard';
+import AdminProducts from './pages/admin-view/AdminProducts';
+import AdminOrders from './pages/admin-view/AdminOrders';
 
 function App() {
   return (
@@ -12,6 +16,11 @@ function App() {
         <Route path="/auth" element={<AuthLayout />}>
           <Route path="login" element={<LoginPage />} />
           <Route path="register" element={<RegisterPage />} />
+        </Route>
+        <Route path="/admin" element={<AdminLayout />}>
+          <Route path="dashboard" element={<AdminDashboard />} />
+          <Route path="products" element={<AdminProducts />} />
+          <Route path="orders" element={<AdminOrders />} />
         </Route>
       </Routes>
     </div>
